e5395e3d6d312b4c9a674dd95a9259d64f6ae42e
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-25 23:16:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(array)
-  count=0 
-  collection =[]
-  while count < array.length 
-  collection <<
-  yield (array[count])
-  count +=1 
+  count = 0 
+  collection=[]
+  while count < array.length
+  collection<< yield(array[count])
+  count+=1 
 end
 collection
 end
-
